be9d59474721462583ec87be8560deb0563f9fdd
ZHU-student <zhytommc@gmail.com>
2024-04-21 16:44:11 +0900

    homework2.py
    i

---
+++ 2024_spring/lec02/my_first_script.py
@@ -0,0 +1,18 @@
+"""
+This is a docstring
+It explains what the code should do,and how to use it.
+
+my_first_script:
+print "this is some text"
+"""
+
+# this is a comment
+
+
+print("this is some text")
+print(5+6)
+print(9-4)
+print(9/3)
+print(5**3)   #5 to the 3rd power
+print(13%5)   #13 modluo 5
+print(13//5) #13 divided by 5 without fractional part (integer)
